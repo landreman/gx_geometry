854ebaf917e3d0441fb5b644a7144fef51138383
Matt Landreman <mattland@umd.edu>
2023-09-05 13:46:50 -0400

    Added driver

--- gx_geometry/__init__.py
+++ gx_geometry/__init__.py
@@ -1,10 +1,12 @@
+from .driver import *
 from .gx_grid import *
 from .util import *
 from .vmec import *
 from .vmec_diagnostics import *
 
 __all__ = [
-    gx_grid.__all__
+    driver.__all__
+    + gx_grid.__all__
     + util.__all__
     + vmec.__all__ 
     + vmec_diagnostics.__all__
---
+++ gx_geometry/driver.py
@@ -0,0 +1,16 @@
+import numpy as np
+from .gx_grid import uniform_arclength, add_gx_definitions, write_eik
+from .vmec import Vmec
+from .vmec_diagnostics import vmec_fieldlines
+
+__all__ = ["create_eik"]
+
+def create_eik(filename, kxfac=1, s=0.64, alpha=0, nl=49):
+    vmec = Vmec(filename)
+    theta1d = np.linspace(-np.pi, np.pi, nl)
+    fl1 = vmec_fieldlines(vmec, s, alpha, theta1d=theta1d)
+    fl2 = uniform_arclength(fl1)
+    kxfac = -1.0
+    add_gx_definitions(fl2, kxfac)
+    eik_filename = "eik.out"
+    write_eik(fl2, eik_filename)
