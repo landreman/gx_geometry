c76418b1f91e18af90e8c444f659501aaec0495b
Matt Landreman <mattland@umd.edu>
2023-09-16 07:50:52 -0400

    Added desc test

--- gx_geometry/desc.py
+++ gx_geometry/desc.py
@@ -1,17 +1,18 @@
 import numpy as np
 from desc.grid import Grid, LinearGrid
+from desc.compute.utils import cross, dot
 from .util import Struct
 
 def desc_fieldline(eq, s, alpha, theta1d):
+    psi = eq.Psi / (2 * np.pi)
     rho = np.sqrt(s)
-    flux_function_keys = ["iota", "iota_r", "p_r", "psi", "a"]
-    linear_grid = LinearGrid(rho=rho, M=64, N=65, NFP=eq.NFP)
+    flux_function_keys = ["iota", "iota_r", "p_r", "a"]
+    linear_grid = LinearGrid(rho=rho, M=34, N=35, NFP=eq.NFP)
     flux_functions = eq.compute(flux_function_keys, grid=linear_grid)
 
     iota = linear_grid.compress(flux_functions["iota"])
     iota_r = linear_grid.compress(flux_functions["iota_r"])
     shat = -(rho / iota) * iota_r
-    print(flux_functions)
 
     nl = len(theta1d)
     theta = theta1d
@@ -19,33 +20,41 @@ def desc_fieldline(eq, s, alpha, theta1d):
     # alpha = theta - iota * zeta
     # zeta = (theta - alpha) / iota
     zeta = (theta - alpha) / iota
+    phi = zeta
     rhoa = rho * np.ones(nl)
     c = np.vstack([rhoa, theta, zeta]).T
     coords = eq.compute_theta_coords(c, tol=1e-10, maxiter=50)
-    print("coords:")
-    print(coords)
     theta_desc = coords[:, 1]
+    theta_vmec = theta_desc
     grid = Grid(coords)
 
     field_line_keys = [
-        "|B|",
+        "|B|", "|grad(psi)|^2", "grad(|B|)", "grad(alpha)", "grad(psi)",
+        "B", "grad(|B|)", "kappa",
     ]
     data = eq.compute(field_line_keys, grid=grid)
 
-    psi = flux_functions['psi'][-1]
     #normalizations       
     L_reference = flux_functions['a']
     B_reference = 2 * np.abs(psi) / (L_reference**2)
-    #calculate bmag
-    modB = data['|B|']
-    bmag = modB / B_reference
 
+    modB = data['|B|']
+    grad_psi_dot_grad_psi = data["|grad(psi)|^2"]
+    grad_alpha_dot_grad_psi = dot(data["grad(alpha)"], data["grad(psi)"])
+    grad_alpha_dot_grad_alpha = dot(data["grad(alpha)"], data["grad(alpha)"])
+
+    B_cross_grad_B_dot_grad_psi = dot(cross(data["B"], data["grad(|B|)"]), data["grad(psi)"])
+    B_cross_grad_B_dot_grad_alpha = dot(cross(data["B"], data["grad(|B|)"]), data["grad(alpha)"])
+    B_cross_kappa_dot_grad_psi = dot(cross(data["B"], data["kappa"]), data["grad(psi)"])
+    B_cross_kappa_dot_grad_alpha = dot(cross(data["B"], data["kappa"]), data["grad(alpha)"])
 
     fl = Struct()
     variables = [
-        "s", "rho", "nl", "theta_pest", "theta_desc", "zeta",
-        "iota", "shat",
-        "bmag"
+        "s", "rho", "nl", "theta_pest", "theta_desc", "theta_vmec", "zeta", "phi",
+        "iota", "shat", "B_reference", "L_reference",
+        "modB", "grad_psi_dot_grad_psi", "grad_alpha_dot_grad_psi", "grad_alpha_dot_grad_alpha",
+        "B_cross_grad_B_dot_grad_psi", "B_cross_grad_B_dot_grad_alpha",
+        "B_cross_kappa_dot_grad_psi", "B_cross_kappa_dot_grad_alpha",
     ]
     for v in variables:
         fl.__setattr__(v, eval(v))
--- tests/test_desc.py
+++ tests/test_desc.py
@@ -5,6 +5,7 @@ import os
 import logging
 
 import numpy as np
+import matplotlib.pyplot as plt
 from desc.vmec import VMECIO
 
 from gx_geometry.desc import desc_fieldline
@@ -30,13 +31,61 @@ class Tests(unittest.TestCase):
         vmec = Vmec(filename)
         fl2 = vmec_fieldlines(vmec, s, alpha, theta1d=theta1d)
 
+        if False:
+            plt.figure(figsize=(14, 7.5))
+            nrows = 3
+            ncols = 4
+            jplot = 1
+            def compare_vmec_desc(field, signflip=1):
+                nonlocal jplot
+                plt.subplot(nrows, ncols, jplot)
+                jplot = jplot + 1
+                plt.plot(fl1.theta_pest, fl1.__getattribute__(field), label="desc")
+                plt.plot(fl2.theta_pest, fl2.__getattribute__(field) * signflip, label="vmec")
+                plt.xlabel("theta_pest")
+                plt.title(field)
+            compare_vmec_desc("modB", signflip=1)
+            compare_vmec_desc("theta_vmec", signflip=1)
+            compare_vmec_desc("phi", signflip=-1)
+            compare_vmec_desc("grad_psi_dot_grad_psi", signflip=1)
+            compare_vmec_desc("grad_alpha_dot_grad_psi", signflip=-1)
+            compare_vmec_desc("grad_alpha_dot_grad_alpha", signflip=1)
+            compare_vmec_desc("B_cross_grad_B_dot_grad_psi", signflip=1)
+            compare_vmec_desc("B_cross_kappa_dot_grad_psi", signflip=1)
+            compare_vmec_desc("B_cross_grad_B_dot_grad_alpha", signflip=-1)
+            compare_vmec_desc("B_cross_kappa_dot_grad_alpha", signflip=-1)
+            plt.tight_layout()
+            plt.legend(loc=0, fontsize=7)
+            plt.show()
+
+        np.set_printoptions(linewidth=400)
+        def show_diffs(field1, field2):
+            print("abs diff:", np.abs(field1 - field2))
+            print("rel diff:", np.abs(field1 - field2) / (0.5 * (field1 + field2)))
+            plt.figure()
+            plt.semilogy(np.abs(field1 - field2), label="abs")
+            plt.plot(np.abs((field1 - field2) / (0.5 * (field1 + field2))), label="rel")
+            plt.legend(loc=0, fontsize=6)
+            plt.show()
+
         # Jacobian of (rho, theta, phi) is negative for vmec, positive for desc,
         # as theta_desc = -theta_vmec. Therefore iota flips sign.
         np.testing.assert_allclose(fl1.iota, -fl2.iota)
         np.testing.assert_allclose(fl1.shat, fl2.shat, rtol=0.0002)
         np.testing.assert_allclose(fl1.theta_pest, fl2.theta_pest)
-        np.testing.assert_allclose(fl1.theta_desc, fl2.theta_vmec)
+        np.testing.assert_allclose(fl1.theta_desc, fl2.theta_vmec, rtol=0.006)
         np.testing.assert_allclose(fl1.zeta, -fl2.phi)
+        np.testing.assert_allclose(fl1.modB, fl2.modB, rtol=0.008)
+        np.testing.assert_allclose(fl1.L_reference, fl2.L_reference)
+        np.testing.assert_allclose(fl1.B_reference, fl2.B_reference)
+        np.testing.assert_allclose(fl1.grad_psi_dot_grad_psi, fl2.grad_psi_dot_grad_psi, rtol=0.01)
+        np.testing.assert_allclose(fl1.grad_alpha_dot_grad_psi, -fl2.grad_alpha_dot_grad_psi, atol=0.2)
+        np.testing.assert_allclose(fl1.grad_alpha_dot_grad_alpha, fl2.grad_alpha_dot_grad_alpha, atol=0.06)
+        np.testing.assert_allclose(fl1.B_cross_grad_B_dot_grad_psi, fl2.B_cross_grad_B_dot_grad_psi, rtol=0.1, atol=1.3)
+        np.testing.assert_allclose(fl1.B_cross_kappa_dot_grad_psi, fl2.B_cross_kappa_dot_grad_psi, atol=0.04)
+        np.testing.assert_allclose(fl1.B_cross_grad_B_dot_grad_alpha, -fl2.B_cross_grad_B_dot_grad_alpha, atol=0.75)
+        #show_diffs(fl1.B_cross_kappa_dot_grad_alpha, -fl2.B_cross_kappa_dot_grad_alpha)
+        np.testing.assert_allclose(fl1.B_cross_kappa_dot_grad_alpha, -fl2.B_cross_kappa_dot_grad_alpha, atol=0.006)
 
 
 if __name__ == "__main__":
