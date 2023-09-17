fabe8c55bde1e6f1f8c7fce397ce46797c4fbc12
Matt Landreman <mattland@umd.edu>
2023-09-17 15:40:33 +0200

    Added desc driver and tests for drivers

--- gx_geometry/driver.py
+++ gx_geometry/driver.py
@@ -1,16 +1,69 @@
 import numpy as np
+import desc.io
+from .desc import desc_fieldline
 from .gx_grid import uniform_arclength, add_gx_definitions
 from .eik_files import write_eik
 from .vmec import Vmec
 from .vmec_diagnostics import vmec_fieldline
 
-__all__ = ["create_eik"]
+__all__ = ["create_eik_from_vmec", "create_eik_from_desc"]
 
-def create_eik(filename, sigma_Bxy=1.0, s=0.64, alpha=0, nl=49):
+def create_eik_from_vmec(
+        filename, 
+        s=0.64, 
+        alpha=0, 
+        nz=49, 
+        poloidal_turns=1, 
+        sigma_Bxy=-1.0,
+        eik_filename="eik.out"
+    ):
+    r"""Driver to create an eik file that GX can read from a vmec wout file.
+
+    Args:
+        filename: Name of a vmec wout file to read.
+        s: Normalized toroidal flux for the field line.
+        alpha: Field line label, theta_pest - iota * zeta.
+        nz: Number of grid points parallel to the field.
+        poloidal_turns: Number of poloidal turns to cover in the parallel direction.
+        sigma_Bxy: (1 / |B|^2) \vec{B} \cdot \nabla x \times \nabla y, usually -1.
+        eik_filename: Name of the eik file to save.
+    """
     vmec = Vmec(filename)
-    theta1d = np.linspace(-np.pi, np.pi, nl)
+    theta1d = np.linspace(-np.pi * poloidal_turns, np.pi * poloidal_turns, nz)
     fl1 = vmec_fieldline(vmec, s, alpha, theta1d=theta1d)
     fl2 = uniform_arclength(fl1)
     add_gx_definitions(fl2, sigma_Bxy)
-    eik_filename = "eik.out"
+    write_eik(fl2, eik_filename)
+
+def create_eik_from_desc(
+        filename, 
+        s=0.64, 
+        alpha=0, 
+        nz=49, 
+        poloidal_turns=1, 
+        sigma_Bxy=-1.0,
+        eik_filename="eik.out"
+    ):
+    r"""Driver to create an eik file that GX can read from a desc .h5 output file.
+
+    Args:
+        filename: Name of a desc .h5 output file to read.
+        s: Normalized toroidal flux for the field line.
+        alpha: Field line label, theta_pest - iota * zeta.
+        nz: Number of grid points parallel to the field.
+        poloidal_turns: Number of poloidal turns to cover in the parallel direction.
+        sigma_Bxy: (1 / |B|^2) \vec{B} \cdot \nabla x \times \nabla y, usually -1.
+        eik_filename: Name of the eik file to save.
+    """
+    eq = desc.io.load(filename)
+    try:
+        # If an EquilibriumFamily, choose the final equilibrium:
+        eq = eq[-1]
+    except:
+        pass
+
+    theta1d = np.linspace(-np.pi * poloidal_turns, np.pi * poloidal_turns, nz)
+    fl1 = desc_fieldline(eq, s, alpha, theta1d=theta1d)
+    fl2 = uniform_arclength(fl1)
+    add_gx_definitions(fl2, sigma_Bxy)
     write_eik(fl2, eik_filename)
---
+++ tests/test_driver.py
@@ -0,0 +1,27 @@
+import unittest
+import os
+import logging
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from gx_geometry import create_eik_from_vmec, create_eik_from_desc
+
+from . import TEST_DIR
+
+logger = logging.getLogger(__name__)
+#logging.basicConfig(level=logging.INFO)
+
+class Tests(unittest.TestCase):
+    def test_vmec_driver(self):
+        filename_base = "wout_w7x_from_gx_repository.nc"
+        filename = os.path.join(TEST_DIR, filename_base)
+        create_eik_from_vmec(filename)
+
+    def test_desc_driver(self):
+        filename_base = "w7x_from_gx_repository_LMN8.h5"
+        filename = os.path.join(TEST_DIR, filename_base)
+        create_eik_from_desc(filename)
+
+if __name__ == "__main__":
+    unittest.main()
