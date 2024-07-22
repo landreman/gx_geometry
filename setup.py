e81c453a747381bb97fc66b325ac05309ebb805a
Noah Mandell <nrmandell@gmail.com>
2024-07-22 12:35:14 -0400

    install dependencies from requirements.txt in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup, find_packages
 
+def parse_requirements(filename):
+    """ Load requirements from a pip requirements file """
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    return [line.strip() for line in lines if line and not line.startswith('#')]
+
+requirements = parse_requirements('requirements.txt')
+
 setup(
     name="gx_geometry",
     version="0.0.1",
@@ -10,6 +18,6 @@ setup(
     packages=find_packages(),
     entry_points = {
         'console_scripts': ['gx_geometry=gx_geometry.module:run_module'],
-    }
-    # install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+    },
+    install_requires=requirements,
 )
