5d486e57f8ce88c53ba999efe1848dc46b265b90
Noah Mandell <nrmandell@gmail.com>
2024-07-22 13:27:45 -0400

    automate choice of vmec vs desc based on whether eq file is .nc or .h5

--- gx_geometry/module.py
+++ gx_geometry/module.py
@@ -22,6 +22,21 @@ def run_module():
     nz = params.get("Dimensions").get("ntheta")
     npol = params.get("Geometry").get("npol")
     
+    geo_file = params.get("Geometry").get("geo_file", None)
+    if geo_file is None:
+        geo_file = params.get("Geometry").get("vmec_file", None)
+        if geo_file is not None:
+            geo_option = "vmec"
+        else:
+            assert False, "Must specify an equilibrium file via 'geo_file' parameter"
+    else:
+        if geo_file[-3:] == ".nc":
+            geo_option = "vmec"
+        elif geo_file[-3:] == ".h5":
+            geo_option = "desc"
+        else:
+            assert False, f"geo_file = {geo_file} is of unknown type"
+
     if geo_option == "vmec":
         # s = psi/psi_LCFS
         try:
