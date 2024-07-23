11b55d70ebab3c31a4917b4a696b457f4e3d7261
Matt Landreman <mattland@umd.edu>
2024-07-23 09:57:33 -0400

    Tweak to module test

--- tests/test_module.py
+++ tests/test_module.py
@@ -8,10 +8,10 @@ from . import TEST_DIR
 
 class Tests(unittest.TestCase):
     def test_run_module(self):
+        """Just run the module and make sure it doesn't crash."""
         cwd = os.getcwd()
         os.chdir(TEST_DIR)
 
-        print(os.path.join(TEST_DIR, "w7x_adiabatic_electrons_vmec.in"))
         run_module([0, "w7x_adiabatic_electrons_vmec.in"])
         run_module([0, "w7x_adiabatic_electrons_desc.in"])
         run_module([0, "w7x_adiabatic_electrons_vmec.in", "w7x.eik.nc"])
